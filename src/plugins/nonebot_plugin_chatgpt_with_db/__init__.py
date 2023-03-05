536b9e9880e73f9dee017b7e532cbb3a92ec8d18
duang <69333241+etienne0519@users.noreply.github.com>
2023-03-06 00:04:03 +0800

    feat: use api of gpt3.5

--- src/plugins/nonebot_plugin_chatgpt_with_db/__init__.py
+++ src/plugins/nonebot_plugin_chatgpt_with_db/__init__.py
@@ -1,7 +1,7 @@
 from nonebot import get_driver
 
 from .config import Config
-from .use_bot import *
+from .nonebot_plugin_chatgpt_with_db import *
 
 global_config = get_driver().config
 config = Config.parse_obj(global_config)
