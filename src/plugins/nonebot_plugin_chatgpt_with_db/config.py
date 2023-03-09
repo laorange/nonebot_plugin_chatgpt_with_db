c94571de4e5717319e31a50d91025141e3652b39
辣橙 <68316902+laorange@users.noreply.github.com>
2023-03-09 11:29:30 +0800

    perf: 调整 config 中的常量设置 (+OVER_BUFFERING)

--- src/plugins/nonebot_plugin_chatgpt_with_db/config.py
+++ src/plugins/nonebot_plugin_chatgpt_with_db/config.py
@@ -1,10 +1,22 @@
 from pydantic import BaseModel, Extra
 
+# 是否为调试模式，不便调用chatgpt接口时调试使用，将不会调用ChatGPT接口
 DEBUG = False
 
+# $ → ¥ 汇率
+EXCHANGE_RATE = 6.93
+
+# 每个token的价格 (RMB)
+PRICE_PER_TOKEN = 0.002 * EXCHANGE_RATE / 1000
+
+# 选用模型的名称
+DEFAULT_MODEL = "gpt-3.5-turbo"
+
+# 模型的最大token数
 MAX_TOKENS = 4096
-PRICE_PER_TOKEN = 0.002 * 6.93 / 1000
-DEFAULT_MODEL = "gpt-3.5-turbo-0301"
+
+# 实际提交的 max_tokens = MAX_TOKENS - (英文字符长度 + 2 * 非英文字符长度 + OVER_BUFFERING)
+OVER_BUFFERING = 200
 
 
 class Config(BaseModel, extra=Extra.ignore):
--- src/plugins/nonebot_plugin_chatgpt_with_db/use_chatgpt/get_response_of_chatgpt.py
+++ src/plugins/nonebot_plugin_chatgpt_with_db/use_chatgpt/get_response_of_chatgpt.py
@@ -5,7 +5,7 @@ import openai
 from pydantic import ValidationError
 
 from .util_types import ChatAccount, ChatGptResponse, ModelCandidate
-from ..config import MAX_TOKENS
+from ..config import MAX_TOKENS, OVER_BUFFERING
 
 
 async def get_response_of_chatgpt(model: ModelCandidate, account: ChatAccount, content: str) -> ChatGptResponse:
@@ -15,16 +15,16 @@ async def get_response_of_chatgpt(model: ModelCandidate, account: ChatAccount, c
 
     alphanumeric_length = len(re.findall(r'[a-zA-Z0-9]', content))
     non_alphanumeric_length = len(content) - alphanumeric_length
-    left_max_tokens = MAX_TOKENS - alphanumeric_length - non_alphanumeric_length * 2
+    real_max_tokens = MAX_TOKENS - alphanumeric_length - non_alphanumeric_length * 2 - OVER_BUFFERING
 
     if model.startswith("gpt-3.5-turbo"):
         raw_response = await openai.ChatCompletion.acreate(
             model=model,
             temperature=temperature,
             messages=[{"role": "user", "content": content}],  # role: "system", "assistant", "user",
-            max_tokens=left_max_tokens)
+            max_tokens=real_max_tokens)
     elif model.startswith("text-davinci-003"):
-        raw_response = await openai.Completion.acreate(model=model, prompt=content, temperature=temperature, max_tokens=left_max_tokens)
+        raw_response = await openai.Completion.acreate(model=model, prompt=content, temperature=temperature, max_tokens=real_max_tokens)
     else:
         raise NotImplementedError(f"暂不支持该模型：{model}")
 
